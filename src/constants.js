db603470f8eb0e14de8d1fce697bf9d5c0a63de7
dhrxv03 <dhruv0310@icloud.com>
2024-09-18 15:35:46 +0530

    Dialouges

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,34 @@
 export const scaleFactors = 4;
 
 export const dialogueData = {
-  pc: `This is my PC where I buit this website aswell using KaboomJS and Vite. I am learning Game Development.
-  I've also been making Apps in Swift and Websites using Angular, React, Next JS. I also like to create 3D Models. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/dhrxv03" target="_blank">Github</a> !`,
-    mail: `Dhruv Shah, <br>
-    ABC Street, INDIA <br>
-    Email:
-    Here is my <a href="mailto:dhruv0310@icloud.com" target="_blank">dhruv0310@icloud.com</a>!`,
+  pc: `This is the PC where I developed a website using KaboomJS and Vite as part of my learning journey in game development. 
+  Additionally, I have been working on applications using Swift and developing websites with frameworks such as Angular, React, and Next.js. 
+  I also have a passion for creating 3D models. 
+  Regardless of the programming language, I have a strong interest in software development. You can explore my work on my <a href="https://github.com/dhrxv03" target="_blank">Github profile</a>.`,
+  mail: `Dhruv Shah, <br>
+  INDIA <br>
+  Email:
+  <a href="mailto:dhruv0310@icloud.com" target="_blank">dhruv0310@icloud.com</a>`,
+  library: `I have a hobby of reading books and collecting them. <br>
+  I have read books on various topics, including programming, self-help, and fiction. <br>
+  Few of my favorite books are: <br>
+  Surrounded by Idiots by Thomas Erikson <br>
+  The Day I Stopped Drinking Milk by Sudha Murthy <br>
+  Percy Jackson Novel Series by Rick Riordian`,
+  "cs-degree": `I am currently in my final year pursuing a Bachelor's degree in Computer Science, with a minor in Cyber Security. 
+  Additionally, I have completed a diploma in Computer Engineering. 
+  I have also undertaken several online courses on platforms such as Coursera and Udemy, covering topics like Cyber Security, Machine Learning, Object-Oriented Programming, and Game Development.`,
+  bio: `Hello! My name is Dhruv Shah, and I am currently pursuing a Bachelor's degree in Computer Science with a strong passion for both game and app development. 
+  This website serves as a platform to showcase my projects and areas of interest. 
+  As a self-motivated programmer, I thrive on exploring new technologies and consistently enjoy building innovative solutions. <br><br>
+  I am a dedicated and hardworking student actively seeking opportunities to apply my skills. 
+  My ability to quickly learn and adapt to new software allows me to work efficiently and effectively in diverse technical environments`,
+  resume: `Feel free to take a look at my <a href='https://github.com/dhrxv03' target='_blank'>resume</a>, 
+  and you're welcome to connect with me on <a href='https://www.linkedin.com/in/dhruvshah03/' target='_blank'>LinkedIn</a>. 
+  I look forward to the opportunity to connect!`,
+  pool: `Swimming has been a beloved hobby of mine since childhood. 
+  Though I am not a professional swimmer, I make it a point to swim every morning, which keeps me fit and rejuvenated. 
+  It not only refreshen me but also helps me stay active and energized throughout the day.`, 
+  bed: `Obviously, I sleep on this bed.`,
+  tv: `I enjoy watching movies in my free time.`,
 }; 
